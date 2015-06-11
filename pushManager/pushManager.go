daf646e6d78d30e8d4c34fed45ece955a2e654ba
Phil <zgrubby@gmail.com>
2015-06-11 16:34:42 +0800

    Fully implement all 3.0 push rest api

--- pushManager/pushManager.go
+++ pushManager/pushManager.go
@@ -127,10 +127,18 @@ func (p *PushManager) QueryTimerRecords(timerId, start, limit, rangeStart, range
 	return postURL(targetURL, dic)
 }
 
-func (p *PushManager) QueryTopicRecords(topicId, start, limit, rangeStart, rangeEnd string) (resp map[string]interface{}, err error) {
+func (p *PushManager) QueryTopicDetails(start, limit, rangeStart, rangeEnd string) (resp map[string]interface{}, err error) {
 	targetURL := "http://api.tuisong.baidu.com/rest/3.0/report/query_topic_records"
 	dic := make(map[string]string)
 	dic["topic_id"] = topicId
+	p.applyBaseParameters(dic)
+	dic["sign"] = util.GenerateSignature("POST", targetURL, p.secretKey, dic)
+	return postURL(targetURL, dic)
+}
+
+func (p *PushManager) QueryTopicRecords(start, limit, rangeStart, rangeEnd string) (resp map[string]interface{}, err error) {
+	targetURL := "http://api.tuisong.baidu.com/rest/3.0/report/query_topic_records"
+	dic := make(map[string]string)
 	if start != "" {
 		dic["start"] = start
 	}
@@ -149,56 +157,133 @@ func (p *PushManager) QueryTopicRecords(topicId, start, limit, rangeStart, range
 	return postURL(targetURL, dic)
 }
 
-func (p *PushManager) QueryTagDetails(tag string) {
-
+func (p *PushManager) QueryTagDetails(tag string) (resp map[string]interface{}, err error) {
+	targetURL := "http://api.tuisong.baidu.com/rest/3.0/app/query_tags"
+	dic := make(map[string]string)
+	dic["tag"] = tag
+	p.applyBaseParameters(dic)
+	dic["sign"] = util.GenerateSignature("POST", targetURL, p.secretKey, dic)
+	return postURL(targetURL, dic)
 }
 
-func (p *PushManager) QueryTags(start, limit string) {
-
+func (p *PushManager) QueryTags(start, limit string) (resp map[string]interface{}, err error) {
+	targetURL := "http://api.tuisong.baidu.com/rest/3.0/app/query_tags"
+	dic := make(map[string]string)
+	dic["start"] = start
+	dic["limit"] = limit
+	p.applyBaseParameters(dic)
+	dic["sign"] = util.GenerateSignature("POST", targetURL, p.secretKey, dic)
+	return postURL(targetURL, dic)
 }
 
-func (p *PushManager) CreateTag(tag string) {
-
+func (p *PushManager) CreateTag(tag string) (resp map[string]interface{}, err error) {
+	targetURL := "http://api.tuisong.baidu.com/rest/3.0/app/create_tag"
+	dic := make(map[string]string)
+	dic["tag"] = tag
+	p.applyBaseParameters(dic)
+	dic["sign"] = util.GenerateSignature("POST", targetURL, p.secretKey, dic)
+	return postURL(targetURL, dic)
 }
 
-func (p *PushManager) DeleteTag(tag string) {
-
+func (p *PushManager) DeleteTag(tag string) (resp map[string]interface{}, err error) {
+	targetURL := "http://api.tuisong.baidu.com/rest/3.0/app/del_tag"
+	dic := make(map[string]string)
+	dic["tag"] = tag
+	p.applyBaseParameters(dic)
+	dic["sign"] = util.GenerateSignature("POST", targetURL, p.secretKey, dic)
+	return postURL(targetURL, dic)
 }
 
-func (p *PushManager) AddDevicesToTag(tag string, channelIds []string) {
-
+func (p *PushManager) AddDevicesToTag(tag string, channelIds []string) (resp map[string]interface{}, err error) {
+	targetURL := "http://api.tuisong.baidu.com/rest/3.0/tag/add_devices"
+	dic := make(map[string]string)
+	channels, err := json.Marshal(&channelIds)
+	if err != nil {
+		return
+	}
+	dic["channel_ids"] = string(channels)
+	dic["tag"] = tag
+	p.applyBaseParameters(dic)
+	dic["sign"] = util.GenerateSignature("POST", targetURL, p.secretKey, dic)
+	return postURL(targetURL, dic)
 }
 
-func (p *PushManager) DeleteDevicesFromTag(tag string, channelIds []string) {
-
+func (p *PushManager) DeleteDevicesFromTag(tag string, channelIds []string) (resp map[string]interface{}, err error) {
+	targetURL := "http://api.tuisong.baidu.com/rest/3.0/tag/del_devices"
+	dic := make(map[string]string)
+	channels, err := json.Marshal(&channelIds)
+	if err != nil {
+		return
+	}
+	dic["channel_ids"] = string(channels)
+	dic["tag"] = tag
+	p.applyBaseParameters(dic)
+	dic["sign"] = util.GenerateSignature("POST", targetURL, p.secretKey, dic)
+	return postURL(targetURL, dic)
 }
 
-func (p *PushManager) QueryNumberOfDevicesInTag(tag string) {
-
+func (p *PushManager) QueryNumberOfDevicesInTag(tag string) (resp map[string]interface{}, err error) {
+	targetURL := "http://api.tuisong.baidu.com/rest/3.0/tag/device_num"
+	dic := make(map[string]string)
+	dic["tag"] = tag
+	p.applyBaseParameters(dic)
+	dic["sign"] = util.GenerateSignature("POST", targetURL, p.secretKey, dic)
+	return postURL(targetURL, dic)
 }
 
-func (p *PushManager) QueryTimerDetails(timerId string) {
-
+func (p *PushManager) QueryTimerDetails(timerId string) (resp map[string]interface{}, err error) {
+	targetURL := "http://api.tuisong.baidu.com/rest/3.0/timer/query_list"
+	dic := make(map[string]string)
+	dic["timer_id"] = timerId
+	p.applyBaseParameters(dic)
+	dic["sign"] = util.GenerateSignature("POST", targetURL, p.secretKey, dic)
+	return postURL(targetURL, dic)
 }
 
-func (p *PushManager) QueryTimerList(start, limit string) {
-
+func (p *PushManager) QueryTimerList(start, limit string) (resp map[string]interface{}, err error) {
+	targetURL := "http://api.tuisong.baidu.com/rest/3.0/timer/query_list"
+	dic := make(map[string]string)
+	dic["start"] = start
+	dic["limit"] = limit
+	p.applyBaseParameters(dic)
+	dic["sign"] = util.GenerateSignature("POST", targetURL, p.secretKey, dic)
+	return postURL(targetURL, dic)
 }
 
-func (p *PushManager) CancelTimerTask(timerId string) {
-
+func (p *PushManager) CancelTimerTask(timerId string) (resp map[string]interface{}, err error) {
+	targetURL := "http://api.tuisong.baidu.com/rest/3.0/timer/cancel"
+	dic := make(map[string]string)
+	dic["timer_id"] = timerId
+	p.applyBaseParameters(dic)
+	dic["sign"] = util.GenerateSignature("POST", targetURL, p.secretKey, dic)
+	return postURL(targetURL, dic)
 }
 
-func (p *PushManager) QueryTopicList(start, limit string) {
-
+func (p *PushManager) QueryTopicList(start, limit string) (resp map[string]interface{}, err error) {
+	targetURL := "http://api.tuisong.baidu.com/rest/3.0/topic/query_list"
+	dic := make(map[string]string)
+	dic["start"] = start
+	dic["limit"] = limit
+	p.applyBaseParameters(dic)
+	dic["sign"] = util.GenerateSignature("POST", targetURL, p.secretKey, dic)
+	return postURL(targetURL, dic)
 }
 
-func (p *PushManager) QueryDeviceStatistic() {
-
+func (p *PushManager) QueryDeviceStatistic() (resp map[string]interface{}, err error) {
+	targetURL := "http://api.tuisong.baidu.com/rest/3.0/report/statistic_device"
+	dic := make(map[string]string)
+	p.applyBaseParameters(dic)
+	dic["sign"] = util.GenerateSignature("POST", targetURL, p.secretKey, dic)
+	return postURL(targetURL, dic)
 }
 
-func (p *PushManager) QueryTopicStatistic(topicId string) {
-
+func (p *PushManager) QueryTopicStatistic(topicId string) (resp map[string]interface{}, err error) {
+	targetURL := "http://api.tuisong.baidu.com/rest/3.0/report/statistic_topic"
+	dic := make(map[string]string)
+	dic["topic_id"] = topicId
+	p.applyBaseParameters(dic)
+	dic["sign"] = util.GenerateSignature("POST", targetURL, p.secretKey, dic)
+	return postURL(targetURL, dic)
 }
 
 func postURL(targetURL string, dic map[string]string) (resp map[string]interface{}, err error) {
