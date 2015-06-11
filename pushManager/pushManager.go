da85ad4734fde8288db0a5d4c943baf5b593c68a
Phil <zgrubby@gmail.com>
2015-06-11 15:46:40 +0800

    Push to single device

--- pushManager/pushManager.go
+++ pushManager/pushManager.go
@@ -53,6 +53,7 @@ func (p PushManager) PushToAll(device_type, msg_type, msg, deploy_status string,
 	dic := make(map[string]string)
 	dic["device_type"] = device_type
 	dic["msg_type"] = msg_type
+	dic["deploy_status"] = deploy_status
 	p.applyBaseParameters(dic)
 	dic["msg"] = util.BuildMessage(msg, parameters, device_type)
 	dic["sign"] = util.GenerateSignature("POST", targetURL, p.secretKey, dic)
@@ -95,8 +96,53 @@ func (p PushManager) PushToAll(device_type, msg_type, msg, deploy_status string,
 	return
 }
 
-func (p PushManager) PushToSingle(device_type, channel_id, msg_type, msg, deploy_status string, parameters map[string]string) {
+func (p PushManager) PushToSingle(device_type, channel_id, msg_type, msg, deploy_status string, parameters map[string]string) (resp map[string]interface{}, err error) {
+	targetURL := "http://api.tuisong.baidu.com/rest/3.0/push/single_device"
+	dic := make(map[string]string)
+	p.applyBaseParameters(dic)
+	dic["device_type"] = device_type
+	dic["msg_type"] = msg_type
+	dic["deploy_status"] = deploy_status
+	dic["channel_id"] = channel_id
+	dic["msg"] = util.BuildMessage(msg, parameters, device_type)
+	dic["sign"] = util.GenerateSignature("POST", targetURL, p.secretKey, dic)
 
+	form := url.Values{}
+	for k, v := range dic {
+		form.Set(k, v)
+	}
+	client := &http.Client{}
+	req, err := http.NewRequest("POST", targetURL, bytes.NewBufferString(form.Encode()))
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+	req.Header.Add("User-Agent", p.userAgent)
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded;charset=utf-8")
+	req.Header.Add("Content-Length", strconv.Itoa(len(form.Encode())))
+
+	response, err := client.Do(req)
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+	defer response.Body.Close()
+	bd, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+	log.Println(string(bd))
+	err = json.Unmarshal(bd, &resp)
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+	log.Println(resp)
+	if resp["error_code"] != nil {
+		err = errors.New("Push failed")
+	}
+	return
 }
 
 func (p PushManager) PushToTag(device_type, tag, msg_type, msg, deploy_status string, parameters map[string]string) {
