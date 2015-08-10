d644f58d39136a210152775a3d09d155963b79a0
root <root@localhost.localdomain>
2015-08-09 21:22:46 -0500

    modify demo

--- README.md
+++ README.md
@@ -6,4 +6,6 @@
 
 Base on REST api version 3.0
 
+修改Config/config.go 里的API_KEY 和SECRET_KEY
 
+main.go Demo程序，　广播消息
--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"Go_Baidu_Push/config"
+	"Go_Baidu_Push/Config"
 	"Go_Baidu_Push/pushManager"
 	"log"
 )
@@ -10,7 +10,7 @@ func main() {
 	if config.SECRET_KEY == "Your secret key" && config.API_KEY == "Your api key" && config.USERAGENT == "BCCS_SDK/3.0 (GNU/Linux 3.13.0-32-generic x86_64) GO/1.4.2 (Baidu Push Server SDK V3.0.0 )" {
 		log.Fatal("Fill with you own APPID, SECRET_KEY, API_KEY, USERAGENT in config.go, enroll your own app in the push console page if not yet have one!")
 	}
-	pushManager.SharedPushManager().PushToSingle("3", "4090484266476381582", "1", "Hello, world!!", "1", nil)
-	pushManager.SharedPushManager().PushToSingle("4", "5629869620076555597", "1", "Hello, world!!", "1", nil)
+	//pushManager.SharedPushManager().PushToSingle("3", "4090484266476381582", "1", "Hello, world!!", "1", nil)
+	pushManager.SharedPushManager().PushToAll("3", "1", "Test Title", "Hello, world!!", "1", nil)
 
 }
